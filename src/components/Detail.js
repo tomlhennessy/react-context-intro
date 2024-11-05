ebe40dabeb4db024d88e27a4c9cc0ea84bb4aba0
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-05 15:38:33 +1000

    createContext, wrapped App with Context.Provider, consumed context in component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,16 @@
+import React, { useContext } from 'react';
+import { HoroscopeContext } from '../Context/HoroscopeContext';
+
 const Detail = () => {
+  const horoscopesObj = useContext(HoroscopeContext);
+
   return (
     <div className='details'>
       <img
         src='https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg'
         alt=''
       />
-      <h2>Current Sign Name</h2>
+      <h2>{horoscopesObj.sign}</h2>
       <h4>Element: </h4>
       <h4>Traits: </h4>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HoroscopeContext } from './Context/HoroscopeContext';
 
 const Root = () => {
   return (
-      <App />
+      <HoroscopeContext.Provider value={{ sign: "Virgo" }}>
+        <App />
+      </HoroscopeContext.Provider>
   );
 };
 
